cdb12e5c3bf665bdd0243febadd11755857e57b5
jessica.white <jessica.white@myunidays.com>
2018-08-02 08:37:25 +0100

    Initial implementation of unidays-javascript

---
+++ __tests__/calculations.test.js
@@ -0,0 +1,39 @@
+const unidaysTracking = require('../src/unidays');
+
+describe('When calling validateNumber directly', () => {
+	let tracking;
+
+	beforeEach(async () => {
+		tracking = new unidaysTracking('0LTio6iVNaKj861RM9azJQ==', 'GBP', 'Order123', 'ABC123');
+	});
+
+	test('With an integer - The result should be 112.00', () => {
+		let result = tracking._validateNumber(112, 2);
+		expect(result).toBe('112.00');
+	});
+
+	test('With a decimal - The result should be 45.70', () => {
+		let result = tracking._validateNumber(45.70, 2);
+		expect(result).toBe('45.70');
+	});
+
+	test('With a decimal as a string - The result should be 67.08', () => {
+		let result = tracking._validateNumber('67.08', 2);
+		expect(result).toBe('67.08');
+	});
+
+	test('With a number with more than 2 d.p as a string - The result should be 67.01', () => {
+		let result = tracking._validateNumber(67.008, 2);
+		expect(result).toBe('67.01');
+	});
+
+	test('With an empty value - The result should be 0.00', () => {
+		let result = tracking._validateNumber('', 2);
+		expect(result).toBe('0.00');
+	});
+
+	test('With a null value - The result should be 0.00', () => {
+		let result = tracking._validateNumber(null, 2);
+		expect(result).toBe('0.00');
+	});
+});
---
+++ __tests__/initialisation.test.js
@@ -0,0 +1,54 @@
+const unidaysTracking = require('../src/unidays');
+
+describe('When the unidaysTracking method is called', () => {
+	let tracking;
+
+	test('With all parameters - The method should be defined', () => {
+		function methodInit() {
+			tracking = new unidaysTracking('0LTio6iVNaKj861RM9azJQ==', 'GBP', 'Order123', 'ABC123');
+		}
+
+		expect(methodInit).toBeDefined();
+	});
+
+	test('With all parameters in test mode - The method should be defined', () => {
+		function methodInit() {
+			tracking = new unidaysTracking('0LTio6iVNaKj861RM9azJQ==', 'GBP', 'Order123', 'ABC123', true);
+		}
+
+		expect(methodInit).toBeDefined();
+	});
+
+	test('With no partnerId - The method should throw an error', () => {
+		function methodInit() {
+			tracking = new unidaysTracking(null, 'GBP', 'Order123', 'ABC123');
+		}
+
+		expect(methodInit).toThrowError('partnerId is required and cannot be empty');
+	});
+
+	test('With no currency - The method should throw an error', () => {
+		function methodInit() {
+			tracking = new unidaysTracking('0LTio6iVNaKj861RM9azJQ==', null, 'Order123', 'ABC123');
+		}
+
+		expect(methodInit).toThrowError('currency is required and cannot be empty');
+	});
+
+	test('With no transactionId - The method should throw an error', () => {
+		function methodInit() {
+			tracking = new unidaysTracking('0LTio6iVNaKj861RM9azJQ==', 'GBP', null, 'ABC123');
+		}
+
+		expect(methodInit).toThrowError('transactionId is required and cannot be empty');
+	});
+
+	test('With no code - The method should throw an error', () => {
+		function methodInit() {
+			tracking = new unidaysTracking('0LTio6iVNaKj861RM9azJQ==', 'GBP', 'Order123', null);
+		}
+
+		expect(methodInit).toThrowError('code is required and cannot be empty');
+	});
+});
+
